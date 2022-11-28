4dc49339adddc82d0b68d976031c7aece99c898b
MarianaVelazquez <Vxlxzquezp@gmail.com>
2022-11-28 14:44:55 -0600

    update

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Vite App</title>
+  </head>
+  <body>
+    <canvas id="bg"></canvas>
+    <main>
+
+      <header>
+        <h1>James Webb</h1>
+        <p>Esta es la recopilación más exclusiva de fondos de pantalla y fotos del telescopio James Webb de la NASA</p>
+      </header>
+
+
+      <blockquote>
+        <p>La NASA revela las primeras imágenes del telescopio Webb de un universo nunca antes visto</p>
+      </blockquote>
+
+      <section>
+        <h2>Quinteto de Stephan:</h2>
+        <p>
+          La vista de Webb de este grupo compacto de galaxias, ubicadas en la constelación de Pegaso, atravesó el velo de polvo que rodea el centro de una galaxia.
+        </p>
+        <p>
+          Ahora, los científicos pueden obtener una visión poco común, con detalles sin precedentes, de cómo las galaxias que interactúan entre sí desencadenan la formación de estrellas y cómo el gas se altera en estas galaxias.
+        </p>
+      </section>
+
+      <section class="light">
+        <h2>Nebulosa de Carina</h2>
+
+        <p>
+          La mirada de Webb a los “precipicios cósmicos” en la Nebulosa de Carina revela las fases más iniciales y rápidas de formación estelar que previamente habían estado ocultas.
+        </p>
+
+        <h2>Nebulosa del Anillo del Sur</h2>
+
+        <p>
+          Esta nebulosa planetaria, una nube de gas en expansión que rodea a una estrella moribunda, está a unos 2.000 años luz de distancia. Aquí, la poderosa mirada infrarroja 
+          de Webb pone por primera vez a plena vista una segunda estrella agonizante. Desde su nacimiento hasta su muerte como una nebulosa planetaria, Webb puede explorar las capas de polvo y gas que expulsan estrellas que envejecen y que algún día pueden convertirse en una nueva estrella o un nuevo planeta.
+        </p>
+
+      </section>
+
+      <section class="left">
+        <h2>WASP-96b (espectro)</h2>
+        <p>
+          La observación detallada obtenida por Webb de este planeta caliente e “inflado” fuera de nuestro sistema solar revela señales inconfundibles de agua, junto con evidencia de bruma y nubes que no había sido detectada en estudios previos de este planeta.
+        </p>
+
+      </section>
+
+      <blockquote>
+        <p>¡Absolutamente emocionante!</p>
+      </blockquote>
+
+      <blockquote>
+        <p>Gracias por leer.</p>
+      </blockquote>
+
+
+    </main>
+    <script type="module" src="/main.js"></script>
+  </body>
+</html>
